39995ce2ae281d370228eeb3dca09d49d856f97e
Asia K <66960776+asiakay@users.noreply.github.com>
2024-05-30 06:00:45 -0400

    styling

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import styles from "./HeroSection.module.css";
 
 const HeroSection = () => {
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,17 +6,29 @@ import styles from "./Navbar.module.css";
 const Navbar = () => {
   useEffect(() => {
     const hamburger = document.querySelector(`.${styles.hamburger}`);
+    const closeIcon = document.querySelector(`.${styles.closeIcon}`);
     const navLinks = document.querySelector(`.${styles.navlinks}`);
 
-    hamburger?.addEventListener("click", () => {
+    const toggleMenu = () => {
       navLinks?.classList.toggle(styles.active);
-    });
+    };
+
+    hamburger?.addEventListener("click", toggleMenu);
+    closeIcon?.addEventListener("click", toggleMenu);
+
+    // Cleanup event listener on component unmount
+
+    return () => {
+      hamburger?.removeEventListener("click", toggleMenu);
+      closeIcon?.removeEventListener("click", toggleMenu);
+    };
   }, []);
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbrand}>Decorator&apos;s Workroom</div>
       <ul className={styles.navlinks}>
+        <li className={styles.closeIcon}>&times;</li>
         <li>
           <a href="#about">About</a>
         </li>
